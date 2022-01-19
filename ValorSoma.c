056da88b437a443d1af82b0f59382628322e7a81
palomacanton <palomacanton04@gmail.com>
2022-01-19 11:35:40 -0300

    first commit

--- ValorSoma.c
+++ ValorSoma.c
@@ -1,13 +1,13 @@
 
-int indice = 13, soma = 0, k = 0;
+#include <stdio.h>
 
 int main(void){
 
-
-    do {
-k = k + 1;
-soma = soma + k;
-}
+int indice = 13, soma = 0, k = 0;
+do {
+    k += k;
+    soma = soma + k;
+    }
 while (k < indice);
 
 printf(soma);
