cc9555ee59aa7a6bd29a53057922114e5dea0a96
palomacanton <palomacanton04@gmail.com>
2022-01-19 23:41:29 -0300

    criei arquivos novos

--- ValorSoma.c
+++ ValorSoma.c
@@ -4,13 +4,15 @@
 int main(void){
 
 int indice = 13, soma = 0, k = 0;
-do {
-    k += k;
-    soma = soma + k;
-    }
-while (k < indice);
 
-printf(soma);
+do{
+    k = k + 1;
+    soma = soma + k;
+    
+} while (k < indice);
+
+
+printf("Soma: %i", soma);
 
 }
 
